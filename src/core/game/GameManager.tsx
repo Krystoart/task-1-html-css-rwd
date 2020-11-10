c12df9b85bce98896eb0f0bb76f12fddda56a59d
krystoart <krystoart@gmail.com>
2020-11-10 18:26:51 +0200

    Added game box styling - centered and margin from top

--- src/core/game/GameManager.tsx
+++ src/core/game/GameManager.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement, useState } from 'react';
 import { Engine } from './GameEngine';
 
+import '../../utils/StyleUtils.scss';
+import './GameManager.scss';
+
 export function GameManager(): ReactElement {
   const [isStarted, setIsStarted] = useState<boolean>(false);
   const [showPlayAgain, setShowPlayAgain] = useState<boolean>(false);
@@ -8,84 +11,86 @@ export function GameManager(): ReactElement {
   const [helperText, setHelperText] = useState<string>('Just make a guess!');
 
   return (
-    <div>
-      <p>
-        Guessing game
-      </p>
-      <p>
-        The computer will generate a random number between 1 and 100 and you,
-        the user, has to guess the number! You have 10 tries to guess the number!
-      </p>
-      {isStarted
-        ? (
-          <div>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="guess">Write your guess here: </label>
-            <input
-              type="text"
-              name="guess"
-              value={currentGuess}
-              onChange={(e) => {
-                const re = /^[0-9\b]+$/;
-                if (e.target.value || re.test(e.target.value)) {
-                  setCurrentGuess(Number(e.target.value));
-                } else {
-                  setCurrentGuess(undefined);
-                }
-              }}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                // TODO it some components don't update in time - replace with useEffect
-                if (currentGuess) {
-                  const result = Engine.guessNumber(currentGuess);
-                  if (result === 1 && !showPlayAgain) {
-                    setHelperText('The guess is too low');
-                  } else if (result === -1 && !showPlayAgain) {
-                    setHelperText('The guess is too high');
-                  } else if (result === 0 && !showPlayAgain) {
-                    setHelperText('The guess is correct! Good job!');
-                    setShowPlayAgain(true);
+    <div className="row align-center">
+      <div className="game-wrapper">
+        <p>
+          Guessing game
+        </p>
+        <p>
+          The computer will generate a random number between 1 and 100 and you,
+          the user, has to guess the number! You have 10 tries to guess the number!
+        </p>
+        {isStarted
+          ? (
+            <div>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label htmlFor="guess">Write your guess here: </label>
+              <input
+                type="text"
+                name="guess"
+                value={currentGuess}
+                onChange={(e) => {
+                  const re = /^[0-9\b]+$/;
+                  if (e.target.value || re.test(e.target.value)) {
+                    setCurrentGuess(Number(e.target.value));
                   } else {
-                    Engine.resetGame();
-                    setShowPlayAgain(true);
+                    setCurrentGuess(undefined);
                   }
-                }
-              }}
-            > Guess!
-            </button>
-            <p>{ helperText }</p>
-            <p>Attempts left: <span>{Engine.attemptsLeft}</span></p>
-          </div>
-        )
-        : (
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                Engine.generateNumber();
-                setIsStarted(true);
-              }}
-            > Start game!
-            </button>
-          </div>
+                }}
+              />
+              <button
+                type="button"
+                onClick={() => {
+                // TODO it some components don't update in time - replace with useEffect
+                  if (currentGuess) {
+                    const result = Engine.guessNumber(currentGuess);
+                    if (result === 1 && !showPlayAgain) {
+                      setHelperText('The guess is too low');
+                    } else if (result === -1 && !showPlayAgain) {
+                      setHelperText('The guess is too high');
+                    } else if (result === 0 && !showPlayAgain) {
+                      setHelperText('The guess is correct! Good job!');
+                      setShowPlayAgain(true);
+                    } else {
+                      Engine.resetGame();
+                      setShowPlayAgain(true);
+                    }
+                  }
+                }}
+              > Guess!
+              </button>
+              <p>{ helperText }</p>
+              <p>Attempts left: <span>{Engine.attemptsLeft}</span></p>
+            </div>
+          )
+          : (
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  Engine.generateNumber();
+                  setIsStarted(true);
+                }}
+              > Start game!
+              </button>
+            </div>
+          )}
+        {showPlayAgain && (
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              Engine.resetGame();
+              Engine.generateNumber();
+              setShowPlayAgain(false);
+              setCurrentGuess(undefined);
+              setHelperText('Just make a guess!');
+            }}
+          > Play again?
+          </button>
+        </div>
         )}
-      {showPlayAgain && (
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            Engine.resetGame();
-            Engine.generateNumber();
-            setShowPlayAgain(false);
-            setCurrentGuess(undefined);
-            setHelperText('Just make a guess!');
-          }}
-        > Play again?
-        </button>
       </div>
-      )}
     </div>
   );
 }
