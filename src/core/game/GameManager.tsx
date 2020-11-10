3583a7c9554a7a3555af66dfee3974a0fb97a3f5
krystoart <krystoart@gmail.com>
2020-11-10 16:16:39 +0200

    Created manager component for game

--- src/core/game/GameManager.tsx
+++ src/core/game/GameManager.tsx
@@ -2,6 +2,15 @@ import React, { ReactElement } from 'react';
 
 export function GameManager(): ReactElement {
   return (
-    <div />
+    <div>
+      <p>
+        Guessing game
+      </p>
+      <p>
+        The computer will generate a random number between 1 and 100 and you,
+        the user, has to guess the number! You have 10 tries to guess the number!
+      </p>
+      <input/>
+    </div>
   );
 }
