7d36ed023aa930f2b9196bc219171d786450d2b8
krystoart <krystoart@gmail.com>
2020-11-10 15:24:52 +0200

    Created Header component for navbar

--- src/core/root/Header.tsx
+++ src/core/root/Header.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import './Header.scss';
+import '../../utils/StyleUtils.scss';
+import { Link } from 'react-router-dom';
+
 export function Header(): React.ReactElement {
   return (
-    <div>
-      kk
-    </div>
+    <nav className="wrapper nav-background-color">
+      <div className="logo" />
+      <div>
+        <div className="button-purple">
+          <Link to={{ pathname: '/game' }}>
+            <p className="text-white">
+              Game
+            </p>
+          </Link>
+        </div>
+        <div className="button">
+          <Link to={{ pathname: '/' }}>
+            <p className="">
+              Home
+            </p>
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
