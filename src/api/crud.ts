f62b67e755e83132655e439d3c09730a574d0a49
krystoart <krystoart@gmail.com>
2020-11-16 21:11:20 +0200

    Added post method

--- src/api/crud.ts
+++ src/api/crud.ts
@@ -14,6 +14,14 @@ app.get('/api/data-all', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/api/data/{:id}', (req, res) => {
+  // res.send("Hello World!");
+});
+
+// app.get("/api", (req, res) => {
+//   res.send("Hello World!");
+// });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
