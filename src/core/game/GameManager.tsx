17ad7f6d138032c1ec999e58abe8cde3e49b73a0
krystoart <krystoart@gmail.com>
2020-11-18 17:54:29 +0200

    Imported `StyleUtils`

--- src/core/game/GameManager.tsx
+++ src/core/game/GameManager.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement, useState } from 'react';
 import { Engine } from './GameEngine';
 
-import '../../utils/StyleUtils.scss';
 import './GameManager.scss';
+import '../../utils/StyleUtils.scss';
 
 export function GameManager(): ReactElement {
   const [isStarted, setIsStarted] = useState<boolean>(false);
@@ -16,104 +16,104 @@ export function GameManager(): ReactElement {
       <div className="game-wrapper">
         <h2 className="title-text">Guessing game</h2>
         <div className="tutorial-text">
-        <p>
+          <p>
             The computer will generate a random number between 1 and 100 and
             you, the user, has to guess the number! You have 10 tries to guess
             the number, good luck!
-        </p>
+          </p>
         </div>
         {isStarted ? (
-            <div>
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label htmlFor="guess">Write your guess here: </label>
-              <input
-                type="text"
-                name="guess"
-                value={currentGuess}
-                onChange={(e) => {
-                  const re = /^[0-9\b]+$/;
-                  if (e.target.value && re.test(e.target.value)) {
-                    setCurrentGuess(e.target.value);
-                  } else if (!e.target.value) {
-                    setCurrentGuess('');
-                  } else {
-                    setCurrentGuess(currentGuess);
-                  }
-                }}
-              />
-              <button
-                type="button"
+          <div>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="guess">Write your guess here: </label>
+            <input
+              type="text"
+              name="guess"
+              value={currentGuess}
+              onChange={(e) => {
+                const re = /^[0-9\b]+$/;
+                if (e.target.value && re.test(e.target.value)) {
+                  setCurrentGuess(e.target.value);
+                } else if (!e.target.value) {
+                  setCurrentGuess('');
+                } else {
+                  setCurrentGuess(currentGuess);
+                }
+              }}
+            />
+            <button
+              type="button"
               className="yellow-button"
-                onClick={() => {
-                  if (currentGuess) {
-                    const result = Engine.guessNumber(Number(currentGuess));
+              onClick={() => {
+                if (currentGuess) {
+                  const result = Engine.guessNumber(Number(currentGuess));
 
-                    if (result || result === 0) {
-                      if (result === 2) {
-                        setHelperText('You lost, too bad :D');
-                        setShowPlayAgain(true);
-                        setAttemptsLeft(Engine.attemptsLeft);
-                        Engine.resetGame();
-                      } else {
-                        if (result === 1) {
-                          setHelperText('The guess is too low');
-                        } else if (result === -1) {
-                          setHelperText('The guess is too high');
-                        } else if (result === 0) {
-                          setHelperText('The guess is correct! Good job!');
-                          setShowPlayAgain(true);
-                        }
-                        setAttemptsLeft(Engine.attemptsLeft);
-                      }
-                    } else {
-                      setHelperText('Some error occured');
+                  if (result || result === 0) {
+                    if (result === 2) {
+                      setHelperText('You lost, too bad :D');
                       setShowPlayAgain(true);
+                      setAttemptsLeft(Engine.attemptsLeft);
                       Engine.resetGame();
+                    } else {
+                      if (result === 1) {
+                        setHelperText('The guess is too low');
+                      } else if (result === -1) {
+                        setHelperText('The guess is too high');
+                      } else if (result === 0) {
+                        setHelperText('The guess is correct! Good job!');
+                        setShowPlayAgain(true);
+                      }
+                      setAttemptsLeft(Engine.attemptsLeft);
                     }
+                  } else {
+                    setHelperText('Some error occured');
+                    setShowPlayAgain(true);
+                    Engine.resetGame();
                   }
-                }}
+                }
+              }}
             >
               {' '}
               Guess!
-              </button>
+            </button>
             <p>{helperText}</p>
             <p>
               Attempts left: <span>{attemptsLeft}</span>
             </p>
-            </div>
+          </div>
         ) : (
-            <div>
-              <button
-                type="button"
+          <div>
+            <button
+              type="button"
               className="yellow-button"
-                onClick={() => {
-                  Engine.generateNumber();
-                  setAttemptsLeft(Engine.attemptsLeft);
-                  setIsStarted(true);
-                }}
+              onClick={() => {
+                Engine.generateNumber();
+                setAttemptsLeft(Engine.attemptsLeft);
+                setIsStarted(true);
+              }}
             >
               {' '}
               Start game!
-              </button>
-            </div>
-          )}
+            </button>
+          </div>
+        )}
         {showPlayAgain && (
-        <div>
-          <button
-            type="button"
+          <div>
+            <button
+              type="button"
               className="yellow-button"
-            onClick={() => {
-              Engine.generateNumber();
-              setShowPlayAgain(false);
-              setCurrentGuess('');
-              setHelperText('Just make a guess!');
-              setAttemptsLeft(Engine.attemptsLeft);
-            }}
+              onClick={() => {
+                Engine.generateNumber();
+                setShowPlayAgain(false);
+                setCurrentGuess('');
+                setHelperText('Just make a guess!');
+                setAttemptsLeft(Engine.attemptsLeft);
+              }}
             >
               {' '}
               Play again?
-          </button>
-        </div>
+            </button>
+          </div>
         )}
       </div>
     </div>
