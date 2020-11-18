21148f590474d52e76a256421e704f1583744092
krystoart <krystoart@gmail.com>
2020-11-18 19:54:08 +0200

    Created comment to show the various available endpoints

--- api/crud.ts
+++ api/crud.ts
@@ -6,8 +6,15 @@ const port = 3000;
 
 // app.get('/api/')
 
-app.get('/api/data', (req, res) => {
-  res.send('Hello World!');
+
+console.log('\n\nEndpoint access: \n'
++ 'To say hello: /api/hello\n'
++ 'Get all data GET: /api/data-all\n'
++ 'Get specific GET: /api/data-get/:id\n'
++ 'Create new elements POST: /api/data-create\n'
++ 'Delete element POST: /api/data-delete\n'
++ 'Update element POST: /api/data-update\n\n');
+
 });
 
 app.get('/api/data-all', (req, res) => {
